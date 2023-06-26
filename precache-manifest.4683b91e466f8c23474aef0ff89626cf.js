c1758bdfdf39711877110a4755fba131fdb2ed56
Shivam-raval96 <sraval@fas.harvard.edu>
2023-06-26 12:45:41 -0400

    Updates

---
+++ precache-manifest.4683b91e466f8c23474aef0ff89626cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e86cbceb3c5cc8ad4bce",
+    "url": "/umap-explorer/static/css/main.9273a056.chunk.css"
+  },
+  {
+    "revision": "e86cbceb3c5cc8ad4bce",
+    "url": "/umap-explorer/static/js/main.e86cbceb.chunk.js"
+  },
+  {
+    "revision": "f6b1676c65765c1e2427",
+    "url": "/umap-explorer/static/js/1.f6b1676c.chunk.js"
+  },
+  {
+    "revision": "abb4f0ea06d4988f39fd",
+    "url": "/umap-explorer/static/js/runtime~main.abb4f0ea.js"
+  },
+  {
+    "revision": "96ad72f92e2883c5b3743bb44f7b4f1b",
+    "url": "/umap-explorer/index.html"
+  }
+];
--- src/Content.js
+++
@@ -1,7 +0,0 @@
-export let about_text = `
-# About EmbedAnything
-This is an interactive visualization that embeds, clusters and analyses multimodal data.
-
-# About UMAP explorer
-The tool is build upon UMAP explorer. The original code is viewable [on github](https://github.com/GrantCuster/umap-explorer).
-`
