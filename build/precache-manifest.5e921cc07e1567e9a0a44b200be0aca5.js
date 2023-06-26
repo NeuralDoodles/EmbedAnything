494e7c17c7a3cd7aeb36ef73956109f6be3cd523
NeuralDoodles <72898964+NeuralDoodles@users.noreply.github.com>
2023-06-26 22:13:58 +0530

    Add files via upload

---
+++ build/precache-manifest.5e921cc07e1567e9a0a44b200be0aca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ec0ea8ed3a4a3aea858",
+    "url": "/EmbedAnything/static/css/main.9273a056.chunk.css"
+  },
+  {
+    "revision": "6ec0ea8ed3a4a3aea858",
+    "url": "/EmbedAnything/static/js/main.6ec0ea8e.chunk.js"
+  },
+  {
+    "revision": "f6b1676c65765c1e2427",
+    "url": "/EmbedAnything/static/js/1.f6b1676c.chunk.js"
+  },
+  {
+    "revision": "8b8df9f1ce5f69a1f1c0",
+    "url": "/EmbedAnything/static/js/runtime~main.8b8df9f1.js"
+  },
+  {
+    "revision": "90d0706864e9154c600383b82d371934",
+    "url": "/EmbedAnything/index.html"
+  }
+];
